d8e1c142637216617fba807596adadb5282f6581
Sayantan Biswas <lordbiswas333@gmail.com>
2026-03-03 11:43:18 +0530

    Initial commit of OOPSBannerAppUC1

---
+++ OOPSBannerAppUC1.java
@@ -0,0 +1,19 @@
+/**
+ * OOPSBannerApp
+ * 
+ * This program prints "OOPS" to the console.
+ * 
+ * @author Sayantan Biswas 
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Entry point of the Java application.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        // Print OOPS to console
+        System.out.println("OOPS");
+    }
+}
